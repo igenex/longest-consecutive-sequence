2f6064536d33b26f9f33a823fb9e9fea2db94137
Eugene <igenexxx@gmail.com>
2018-03-04 20:42:00 +0300

    82 points

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  if(array.length < 2) {
+    return array.length;
+  }
+
+  function InsertionSort(array)
+  {
+    let n = array.length;
+    for (let i = 0; i < n; i++)
+    { let v = array[ i ], j = i-1;
+      while (j >= 0 && array[j] > v)
+      { array[j+1] = array[j]; j--; }
+      array[j+1] = v;
+    }
+    return array;
+  }
+
+  InsertionSort(array);
+
+  let prevNumber = array[0],
+      maxSequence = [],
+      i = 0,
+      last = true;
+
+  for (let j = 1, max = array.length; j < max; j++) {
+   if(array[j] === prevNumber + 1) {
+     if(!last) last = true;
+     if(!maxSequence[i]) maxSequence[i] = 1;
+     maxSequence[i]++;
+   } else {
+     if(last) {
+       i++;
+       last = false;
+     }
+   }
+    prevNumber = array[j];
+  }
+
+  if(!maxSequence[0]) {
+    maxSequence.shift();
+  }
+  return maxSequence.length > 0 ? Math.max(...maxSequence) : 0;
+
+};
